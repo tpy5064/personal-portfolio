1e25b53d15f0f2fa66c708361704dfab0b769d4a
tpy5064 <tpy5064@psu.edu>
2021-07-14 15:17:50 +0800

    initial commit with navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaUser, FaUserGraduate, FaMedal, FaPhone } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="logo">
+          <a href="#" className="nav-link">
+            <span className="link-text logo-text">YTQ</span>
+            <MdKeyboardArrowRight className="logo-icon" />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <FaUser className="nav-icons glow" />
+            <span className="link-text glow">About</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <FaMedal className="nav-icons glow" />
+            <span className="link-text glow">Experience</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <FaUserGraduate className="nav-icons glow" />
+            <span className="link-text glow">Education</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <FaPhone className="nav-icons glow" />
+            <span className="link-text glow">Contact</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <BsThreeDotsVertical className="nav-icons" />
+            <span className="link-text glow">Extras</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
