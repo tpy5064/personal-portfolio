73f347c1b8509385380f819779dad89136b8c5cc
tpy5064 <tpy5064@psu.edu>
2021-07-17 22:12:05 +0800

    Completed inital split screen

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './styles/Navbar.css';
+import './styles/HeroSection.css'
 import App from './App';
 
 ReactDOM.render(
